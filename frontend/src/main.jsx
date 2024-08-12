e323c3dc5c6ebd98ce598bcc288ccabd308964b8
nyambuukim <nyambuukim1@gmail.com>
2024-08-12 10:03:18 +0300

    Configure render path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// Importing the React library
+//
+import React from 'react';
 
+// importing the REactDOM library for rendering React components
+import ReactDOM from 'react-dom/client';
+
+// Import the main App component
+import App from './App.jsx';
+
+
+//Render the App component into the root element of the HTML
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
