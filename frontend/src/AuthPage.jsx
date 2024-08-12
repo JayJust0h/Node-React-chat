004d14efa79facd1e6c98032e1259ea4970ec01c
nyambuukim <nyambuukim1@gmail.com>
2024-08-12 11:22:34 +0300

    AuthPage.Jsx resolve error 403

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -14,12 +14,9 @@ const AuthPage = (props) => {
   
     return (
       <div className="background">
-	    /* Form Element with on Submit Event Handler */
         <form onSubmit={onSubmit} className="form-card">
           <div className="form-title">Welcome 👋</div>
-  
           <div className="form-subtitle">Set a username to get started</div>
-  		/* AUTHENTICATION */
           <div className="auth">
             <div className="auth-label">Username</div>
             <input className="auth-input" name="username" />
@@ -32,5 +29,4 @@ const AuthPage = (props) => {
     );
   };
 
-  // Export AuthPage component as the default export
   export default AuthPage;
