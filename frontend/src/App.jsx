6d7c07336bea25c76bf54c9afd81640e9df002b4
JayJust0h <edwinjustus11@gmail.com>
2024-08-12 09:13:41 +0300

    Add Props parameter  to ChatsPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
 
-import { useState } from "react";
+import { useState } from "react"; // Import useState from react
 
-import "./App.css";
+import "./App.css"; // Import the App.css file
 
-import AuthPage from "./AuthPage";
-import ChatsPage from "./ChatsPage";
+import AuthPage from "./AuthPage"; // Import the AuthPage component
+import ChatsPage from "./ChatsPage"; // Import the ChatsPage component
 
 function App() {
+  // useState is a hook that allows you to add state to a functional component
   const [user, setUser] = useState(undefined);
 
+  // If there is no user, return the AuthPage component
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
   } else {
+    // If there is a user, return the ChatsPage component
     return <ChatsPage user={user} />;
   }
 }
 
-export default App;
+export default App; // Export the App component
