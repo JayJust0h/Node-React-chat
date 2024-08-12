bbbffc60303b171bbe0172d352de9dfaf94a7384
nyambuukim <nyambuukim1@gmail.com>
2024-08-12 11:14:47 +0300

    import app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,17 @@
 
-import { useState } from "react"; // Import useState from react
+import { useState } from "react";
 
-import "./App.css"; // Import the App.css file
+import "./App.css";
 
-import AuthPage from "./AuthPage"; // Import the AuthPage component
-import ChatsPage from "./ChatsPage"; // Import the ChatsPage component
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
 
 function App() {
-  // useState is a hook that allows you to add state to a functional component
   const [user, setUser] = useState(undefined);
 
-  // If there is no user, return the AuthPage component
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
   } else {
-    // If there is a user, return the ChatsPage component
     return <ChatsPage user={user} />;
   }
 }
